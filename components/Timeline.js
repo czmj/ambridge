a594f0bc87a2264705da76d34d93796b8b14f8fe
Clara Jordan <clarazmjordan@gmail.com>
2026-01-23 01:39:02 +0000

    Add timeline styling

--- components/Timeline.js
+++ components/Timeline.js
@@ -5,13 +5,28 @@ export default function Timeline({ episodes, totalCount, currentPage, slug, page
   const totalPages = Math.ceil(totalCount / pageSize);
 
   return (
-    <div className="mt-8">
+    <div className="mt-8 pl-3">
       {!!episodes.length && (
-        <div className="space-y-16">
+        <div className="space-y-16 border-l-4 border-blue-500">
           {episodes.map((ep) => {
 
             return (
-              <div key={ep.episodePid}>
+              <div key={ep.episodePid} className="
+              pl-8
+              relative 
+              before:content-[''] 
+              before:block 
+              before:box-border 
+              before:w-8 
+              before:h-8 
+              before:border-4 
+              before:border-solid 
+              before:border-blue-500
+              before:rounded-full 
+              before:absolute 
+              before:-left-4.5 
+              before:bg-white
+              ">
                 <h2 className="text-2xl font-bold mb-4">
                   {ep.date}
                 </h2>
@@ -45,7 +60,7 @@ export default function Timeline({ episodes, totalCount, currentPage, slug, page
         ) : <div />}
 
         {totalPages ? (<span className="text-gray-400">Page {currentPage} / {totalPages}</span>) : <p>No appearances in archive</p>}
-        
+
 
         {totalPages && currentPage < totalPages ? (
           <Link href={`/character/${slug}?page=${currentPage + 1}`} className="hover:underline">
